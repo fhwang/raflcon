85f917f5d2461796442ce5ca8998c0e1dc17f74f
Francis Hwang <francis@diversionmedia.com>
2009-11-06 12:49:43 -0500

    don't require Giveaway#position

--- app/models/giveaway.rb
+++ app/models/giveaway.rb
@@ -6,8 +6,6 @@ class Giveaway < ActiveRecord::Base
   
   validates_presence_of   :count
   validates_presence_of   :giveaway_round_id
-  validates_presence_of   :position
-  validates_presence_of   :position, :scope => :giveaway_round_id
   validates_presence_of   :prize_category_id
   validates_uniqueness_of :prize_category_id, :scope => :giveaway_round_id
   
