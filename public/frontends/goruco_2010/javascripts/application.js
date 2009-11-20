2c9bafcd94f359744fc09a291b1acb034e30f42f
Francis Hwang <sera@fhwang.net>
2009-11-20 11:39:14 -0800

    front-end

--- public/frontends/goruco_2010/javascripts/application.js
+++ public/frontends/goruco_2010/javascripts/application.js
@@ -1,24 +1,3 @@
-function choose_giveaway_round() {
-  var giveaway_round_id = $('giveaway_round_select').value;
-  if (giveaway_round_id) {
-    var url = '/giveaway_rounds/show/' + giveaway_round_id + '.json';
-    new EJS(
-      {url: "/frontends/goruco_2010/ejs/giveaway_round.ejs"}
-    ).update('giveaway_round', url);
-  }
-};
-
-function choose_giveaway(giveaway_id) {
-  var url = '/giveaways/show/' + giveaway_id + '.json';
-  $$('.giveaway').each(function(elt) {
-    elt.hide();
-  });
-  new EJS(
-    {url: "/frontends/goruco_2010/ejs/giveaway.ejs"}
-  ).update('giveaway_' + giveaway_id, url);
-  $('giveaway_' + giveaway_id).show();
-};
-
 function close_giveaway(giveaway_id) {
   new Ajax.Request(
     '/giveaways/update/' + giveaway_id,
@@ -39,13 +18,52 @@ function close_giveaway(giveaway_id) {
   $('giveaway_' + giveaway_id).hide();
 };
 
+function close_all_giveaway_forms(giveaway_id) {
+  $$('.giveaway').each(function(elt) { elt.hide(); });
+  $$('.arrow_right').each(function(elt) { elt.show(); });
+  $$('.arrow_down').each(function(elt) { elt.hide(); });
+};
+
+function open_giveaway_form(giveaway_id) {
+  close_all_giveaway_forms();
+  var url = '/giveaways/show/' + giveaway_id + '.json';
+  /*
+  $$('.giveaway').each(function(elt) {
+    elt.hide();
+  });
+  */
+  new EJS(
+    {url: "/frontends/goruco_2010/ejs/giveaway.ejs"}
+  ).update('giveaway_form_' + giveaway_id, url);
+  $('giveaway_form_' + giveaway_id).show();
+  $$('#giveaway_' + giveaway_id + " .arrow_right").each(function(elt) {
+    elt.hide();
+  });
+  $$('#giveaway_' + giveaway_id + " .arrow_down").each(function(elt) {
+    elt.show();
+  });
+};
+
 function init_control_panel() {
   new EJS(
     {url: "/frontends/goruco_2010/ejs/control_panel.ejs"}
   ).update(
     'control_panel', "/giveaway_rounds"
   );
+  setTimeout("try_load_giveaway_round()", 25);
 }
+
+function load_giveaway_round() {
+//alert('choose_giveaway_round 0');
+  var giveaway_round_id = $('giveaway_round_select').value;
+//alert(giveaway_round_id);
+  if (giveaway_round_id) {
+    var url = '/giveaway_rounds/show/' + giveaway_round_id + '.json';
+    new EJS(
+      {url: "/frontends/goruco_2010/ejs/giveaway_round.ejs"}
+    ).update('giveaway_round', url);
+  }
+};
 
 function new_giveaway_attempt(giveaway_attempt) {
   $('giveaway_form').hide();
@@ -68,4 +86,12 @@ function parse_w3cdtf(time_str) {
       matches[1], matches[2]-1, matches[3], matches[4], matches[5], matches[6]
     )
   );
+}
+
+function try_load_giveaway_round() {
+  if ($('giveaway_round_select')) {
+    load_giveaway_round();
+  } else {
+    setTimeout("try_load_giveaway_round()", 25);
+  }
 }
