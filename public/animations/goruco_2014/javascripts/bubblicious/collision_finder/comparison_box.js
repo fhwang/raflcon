76fbdcffcd877f4a61518318473ba2a81bfbf928
Francis Hwang <sera@fhwang.net>
2014-05-03 19:15:54 -0400

    WIP: cleanup

---
+++ public/animations/goruco_2014/javascripts/bubblicious/collision_finder/comparison_box.js
@@ -0,0 +1,44 @@
+Bubblicious.CollisionFinder.ComparisonBox = function(xBounds, yBounds) {
+  this.xBounds = xBounds;
+  this.yBounds = yBounds;
+  this.bubbleStates = []
+}
+
+Bubblicious.CollisionFinder.ComparisonBox.prototype = {
+  add: function(bubbleState) {
+    this.bubbleStates.push(bubbleState)
+  },
+
+  colliding: function(bubbleState1, bubbleState2) {
+    return (!bubbleState1.locked || !bubbleState2.locked) && 
+      (bubbleState1.overlaps(bubbleState2))
+  },
+
+  collision: function(bubbleState1, bubbleState2) {
+    return new Bubblicious.Collision.TwoBubble([bubbleState1, bubbleState2])
+  },
+
+  collisions: function() {
+    var self = this,
+        bubbleState1, bubbleState2;
+    result = [];
+    for (var i = 0; i < this.bubbleStates.length; i++) {
+      bubbleState1 = this.bubbleStates[i];
+      for (var j = i + 1; j < this.bubbleStates.length; j++) {
+        bubbleState2 = this.bubbleStates[j];
+        if (this.colliding(bubbleState1, bubbleState2)) {
+          result.push(this.collision(bubbleState1, bubbleState2))
+        }
+      }
+    }
+    return result
+  },
+
+  shouldContain: function(bubbleState) {
+    var location = bubbleState.location
+    return (
+      this.xBounds[0] <= location.x && this.xBounds[1] > location.x &&
+      this.yBounds[0] <= location.y && this.yBounds[1] > location.y
+    )
+  }
+}
